50e1ee0bf76c3a7a5276eab00b4eb6f423a8dc99
ariana124 <ariana.bibiano@gmail.com>
2020-09-13 21:55:06 -0700

    now has routes that handle specific articles

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.listen(3000, function() {
     console.log("Server is running on port 3000.");
 });
 
+/************ Requests targeting all articles *************/
+
 app.route("/articles")
     .get(function(req, res) {
         Article.find(function(err, foundArticles){
@@ -58,5 +60,51 @@ app.route("/articles")
                 res.send("Successfully deleted all articles.");
             }
         });
+    });
+
+/************* Requests targeting specific articles *************/
+
+app.route("/articles/:articleTitle")
+    .get(function(req, res) {
+        const articleTitle = req.params.articleTitle;
+
+        Article.findOne({title: articleTitle}, function(err, foundArticle){
+            if (foundArticle) {
+                res.send(foundArticle);
+            } else {
+                res.send("No articles matching the title were found.");
+            }
+        })
     })
-    
+    .put(function(req, res) {
+        const articleTitle = req.params.articleTitle;
+        const title = req.body.title;
+        const content = req.body.content;
+
+        Article.update(
+            {title: articleTitle},
+            {title: title, content: content},
+            {overwrite: true},
+            function(err){
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send("Successfully updated article.");
+                }
+        })
+    })
+    .patch(function(req, res) {
+        const articleTitle = req.params.articleTitle;
+        
+        Article.update(
+            {title: articleTitle},
+            {$set: req.body},
+            function (err){
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send("Successfully updated article.");
+                }
+            }
+        )
+    })
